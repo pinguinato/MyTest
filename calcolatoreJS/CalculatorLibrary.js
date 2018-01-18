c48409568307832629e30ce5609d6c36f6e05e8a
Roberto Gianotto <gianottoroberto@gmail.com>
2018-01-18 23:37:09 +0100

    Simple JS Calculator development start

---
+++ calcolatoreJS/CalculatorLibrary.js
@@ -0,0 +1,27 @@
+// TODO: scrivere le funzioni
+
+
+window.onload = function(){
+    clickOn7();
+    clickOn8();
+    clickOn9();
+}
+
+
+function clickOn7(){
+    document.getElementById('btn7').onclick = function(){
+        console.log("Cliccato 7");
+    }
+}
+function clickOn8(){
+    document.getElementById('btn8').onclick = function(){
+        console.log("Cliccato 8");
+    }
+}
+function clickOn9(){
+    document.getElementById('btn9').onclick = function(){
+        console.log("Cliccato 9");
+    }
+}
+
+
